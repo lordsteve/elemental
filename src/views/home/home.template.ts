eb847d2a1c092cdb5ecd5d8720cfcb058db6b614
Steve Beaudry <live.remix@gmail.com>
2024-09-30 09:54:49 -0400

    I guess custom elements have to have a dash or something

--- src/views/home/home.template.ts
+++ src/views/home/home.template.ts
@@ -4,13 +4,16 @@ const homeTemplate = (
     welcome: string | null = null
 ) => {
     welcome = !welcome ? '' : htmlstring`<p>${welcome}</p>`;
-    
+
     return html`
-    <main>
-        <h1>Hello World!</h1>
-        ${welcome}
-        <button id="btn"><i class="fa-solid fa-download"></i></button>
-    </main>`;
+    <el-home>
+        <section>
+            <h1>Hello World!</h1>
+            ${welcome}
+            <button id="btn"><i class="fa-solid fa-download"></i></button>
+        </section>
+    </el-home>
+    `;
 }
 
 export default homeTemplate;
