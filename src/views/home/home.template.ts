cb3800083e3e691737107723e14b4752ebdd7c4d
Steve Beaudry <live.remix@gmail.com>
2024-09-30 01:54:17 -0400

    slightly improve home template

--- src/views/home/home.template.ts
+++ src/views/home/home.template.ts
@@ -2,15 +2,15 @@ import { html, htmlstring } from '@const/elements';
 
 const homeTemplate = (
     welcome: string | null = null
-) => html`
-<main>
-    <h1>Hello World!</h1>
-    ${
-        welcome
-        ? htmlstring`<p>${welcome}</p>`
-        : ''
-    }
-    <button id="btn"><i class="fa-solid fa-download"></i></button>
-</main>`;
+) => {
+    welcome = !welcome ? '' : htmlstring`<p>${welcome}</p>`;
+    
+    return html`
+    <main>
+        <h1>Hello World!</h1>
+        ${welcome}
+        <button id="btn"><i class="fa-solid fa-download"></i></button>
+    </main>`;
+}
 
 export default homeTemplate;
